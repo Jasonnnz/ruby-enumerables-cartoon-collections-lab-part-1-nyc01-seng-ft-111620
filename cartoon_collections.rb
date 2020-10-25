e0eb054ac05837d787076182ac986633661e4485
Jason  Zhou <jzhou35@buffalo.edu>
2020-10-25 01:22:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,6 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each_with_index do |elem,index| 
-    puts "#{index}.*#{elem}"
+    puts "#{index + 1}.*#{elem}"
   end
 end
